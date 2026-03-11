ac74df776206405726e988b3b50002032beedac3
Seth Chatterley <seth.chatterley@orderprotection.com>
2026-03-11 12:51:15 -0600

    Fix favicon - use correct Otto branding

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,6 +30,23 @@ export default defineNuxtConfig({
       ],
       link: [
         {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.ico'
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '/favicon-32x32.png'
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '/favicon-16x16.png'
+        },
+        {
           rel: 'preconnect',
           href: 'https://fonts.googleapis.com'
         },
